bc263349284445fb72fb2d0631249e9eec5b32d9
Itamar Bar-Lev <bitamar@gmail.com>
2020-05-06 22:05:06 +0300

    Updates

---
+++ precache-manifest.dae319970d09e22475f49cdc13bf980b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdfb419b9a487606ba3f6a6196219db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344a2a388d42666956de",
+    "url": "/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "2111ceeab2f5a4701d65",
+    "url": "/static/css/main.64e7c554.chunk.css"
+  },
+  {
+    "revision": "344a2a388d42666956de",
+    "url": "/static/js/2.0cf2560f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cf2560f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2111ceeab2f5a4701d65",
+    "url": "/static/js/main.46144a07.chunk.js"
+  },
+  {
+    "revision": "e8948ffb8948f0d9fe9d",
+    "url": "/static/js/runtime-main.1c96a9e1.js"
+  },
+  {
+    "revision": "c5bf09ae25cdc42aa3f24dddd2302145",
+    "url": "/static/media/digital-7.c5bf09ae.woff"
+  },
+  {
+    "revision": "df6f9cd953c8f597a1b18cff5cf3039d",
+    "url": "/static/media/digital-7.df6f9cd9.woff2"
+  }
+]);
